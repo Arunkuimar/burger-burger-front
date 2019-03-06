309a62ad13b181a0797ddc1f45acfef3313dbe88
Arunkuimar <arunachaalamm@gmail.com>
2019-03-07 00:15:25 +0530

    burger initial commit

---
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+import Aux from '../../hoc/Auxillary';
+
+class Burgerbuilder extends Component{
+    render(){
+        return (
+            <Aux>
+            <div>Burger</div>
+            <div>BuildControls</div>
+            </Aux>
+        );
+
+    }
+}
+
+export default Burgerbuilder;
